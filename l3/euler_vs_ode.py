38d4b1f093bd24585771371ef2b4dcae33375733
maybork <maybork@pm.me>
2022-05-04 15:12:20 +0200

    pretty print approx error

--- l3/euler_vs_ode.py
+++ l3/euler_vs_ode.py
@@ -1,3 +1,5 @@
+# type:ignore
+
 import euler_lv
 import odeint_lv
 from nsteps import nsteps
@@ -7,17 +9,24 @@ import numpy as np
 PARAMS = (1.2, 0.6, 0.3, 0.8)
 X0 = 2
 Y0 = 1
-TESTED_STEPS = (0.3, 0.01, 0.001)
+TESTED_STEPS = (0.3, 0.1, 0.01, 0.001, 0.0001)
 INTERVAL = 25
 
-for stepsize in TESTED_STEPS:
+print(f"{'step':>6} | {'x':>9} | {'y':>9} | {'(x+y)/2':>9}")
+fig, axs = plt.subplots(nrows=len(TESTED_STEPS), sharex=True, squeeze=True)
+for i, stepsize in enumerate(TESTED_STEPS):
     num_of_steps = nsteps(INTERVAL, stepsize)
     eul, t = euler_lv.calc_euler(INTERVAL, stepsize, X0, Y0, PARAMS)
     ode, t = odeint_lv.calc_odeint(INTERVAL, stepsize, X0, Y0, PARAMS)
     # print(t.shape, eul[:, 0].shape)
     avg_deviation_x = np.average((eul[:, 0] - ode[:, 0]) ** 2)
     avg_deviation_y = np.average((eul[:, 1] - ode[:, 1]) ** 2)
-    print(f"average deviation for step {stepsize}: {avg_deviation_x} {avg_deviation_y}")
-    plt.plot(t, eul[:, 0], "g", label="x(t)")
-    plt.plot(t, ode[:, 0], "r", label="y(t)")
-    plt.show()
+    print(
+        f"{stepsize:6} | {avg_deviation_x:9.6f} | {avg_deviation_y:9.6f} | {(avg_deviation_x+avg_deviation_y)/2:9.6f}"
+    )
+    axs[i].plot(t, eul[:, 0], "g")
+    axs[i].plot(t, ode[:, 0], "r")
+    axs[i].set_title(f"dt = {stepsize}")
+
+plt.suptitle("Porównanie między metodą eulera i scipy.odeint (porównywane wartości x)")
+plt.show()
