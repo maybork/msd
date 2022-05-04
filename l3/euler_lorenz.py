0f63d760666c9a86eb0d6d1337a00a300dd95c3a
maybork <maybork@pm.me>
2022-05-04 21:19:15 +0200

    fix typo

--- l3/euler_lorenz.py
+++ l3/euler_lorenz.py
@@ -12,7 +12,7 @@ def calc_euler(
     z0: int,
     params: tuple[float, float, float],
 ):
-    omega, beta, rho = params
+    sigma, beta, rho = params
     steps = nsteps(interval, dt)
     t = np.linspace(0, interval, steps)
     xyz = np.zeros((steps, 3))
@@ -22,7 +22,7 @@ def calc_euler(
 
     for i in range(np.shape(xyz)[0] - 1):
         x, y, z = xyz[i, 0], xyz[i, 1], xyz[i, 2]
-        dx = omega * (y - x) * dt
+        dx = sigma * (y - x) * dt
         dy = (x * (rho - z) - y) * dt
         dz = ((x * y) - (beta * z)) * dt
         xyz[i + 1, 0] = x + dx
@@ -32,11 +32,11 @@ def calc_euler(
     return xyz, t
 
 
-x0 = y0 = z0 = 1
-omega = 10
-beta = 8 / 3
-rho = 28
-sol, t = calc_euler(25, 0.001, x0, y0, z0, (omega, beta, rho))
-plt.plot(sol[:, 0], sol[:, 2])
+# x0 = y0 = z0 = 1
+# sigma = 10
+# beta = 8 / 3
+# rho = 28
+# sol, t = calc_euler(25, 0.001, x0, y0, z0, (sigma, beta, rho))
+# plt.plot(sol[:, 0], sol[:, 2])
 
-plt.show(block=True)
+# plt.show(block=True)
